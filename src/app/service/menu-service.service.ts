8e588387a92ae5a788e65e818a44ce6128abc895
ansh sharma <anshshrama60oo@gmail.com>
2026-06-01 12:35:55 +0530

    refactor: Update proxy target and clean up unused menu items in MenuServiceService

--- src/app/service/menu-service.service.ts
+++ src/app/service/menu-service.service.ts
@@ -506,27 +506,8 @@ export class MenuServiceService {
               route: '/masters/store-home',
             },
             {
-              label: 'Update Consignee Information',
-              route: '/masters/consignee-information',
-            },
-            {
               label: 'Report-Specification',
               route: '/masters/report-specification',
-            },
-            {
-              label: 'CME EEL Suggestion',
-              route: '/masters/cme-eel-suggestion',
-            },
-            { label: 'Supplier', route: '/masters/supplier' },
-            { label: 'Add New Items', route: '/masters/items' },
-            { label: 'Mapping to Main items', route: '/masters/map-items' },
-            {
-              label: 'Update Mapping to Main items',
-              route: '/masters/map-items-update',
-            },
-            {
-              label: 'Main Equipment Mapped Report',
-              route: '/masters/map-items-report',
             },
           ],
         },
@@ -559,8 +540,6 @@ export class MenuServiceService {
           label: 'Contracts',
           route: '',
           submenu: [
-            { label: 'Contract Dashboard', route: '/contracts/dashboard' },
-            { label: 'New Contract', route: '/contracts/new-rc' },
             {
               label: 'RC Details Report',
               route: '/contracts/rc-detail-report',
@@ -597,22 +576,9 @@ export class MenuServiceService {
           ],
         },
         {
-          label: 'Finance Report',
+          label: 'Reports',
           route: '',
-          submenu: [
-            {
-              label: 'Year Wise PO Abstract',
-              route: '/finance/year-wise-po-abstract',
-            },
-            {
-              label: 'PO Wise Payment FReport',
-              route: '/finance/po-wise-payment',
-            },
-            {
-              label: 'Cheque Wise Payment Report',
-              route: '/finance/cheque-wise-payment',
-            },
-          ],
+          submenu: [{ label: 'CMC Detail', route: '/reports/cmc-detail' }],
         },
         {
           label: 'Complain',
@@ -623,28 +589,6 @@ export class MenuServiceService {
               route: '/complain/facility-store',
             },
             { label: 'Complain Report', route: '/complain/report' },
-          ],
-        },
-        {
-          label: 'Reports',
-          route: '',
-          submenu: [
-            { label: 'CMC Detail', route: '/reports/cmc-detail' },
-            { label: 'PO Summary', route: '/reports/po-summary' },
-            {
-              label: 'PO Wise Payment Report',
-              route: '/reports/po-wise-payment',
-            },
-            { label: 'Tenders Status', route: '/reports/tenders-status' },
-            {
-              label: 'PO Receipts summary',
-              route: '/reports/po-receipts-summary',
-            },
-            {
-              label: 'Item Status in Tenter',
-              route: '/reports/tender-item-status',
-            },
-            { label: 'Reagent PO Report', route: '/reports/reagent-po' },
           ],
         },
       ],
