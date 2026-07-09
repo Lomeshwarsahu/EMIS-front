b21a91e49397233c83e35b6cafce20300110c28d
ansh sharma <anshshrama60oo@gmail.com>
2026-07-09 12:55:16 +0530

    refactor: remove Reticle integration and related test IDs for cleaner codebase

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,6 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
-import { environment } from './environments/environment';
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
-
-if (!environment.production) {
-  void import('./reticle-dev').then(({ initReticle }) => initReticle());
-}
